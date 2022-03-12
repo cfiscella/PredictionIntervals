076f6f646d1d8be2a8a5bcb7ee2c13684b92411b
cfiscella <52965416+cfiscella@users.noreply.github.com>
2022-03-12 00:40:42 -0500

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-raw_data = pd.read_csv('',parse_dates = True,index_col = "DATE",)['2003-12-30':'2021-06-30'].drop("1Y_yield",axis=1)
+raw_data = pd.read_csv('PredictionIntervals/data/raw/raw_full_dataset.csv',parse_dates = True,index_col = "DATE",)['2003-12-30':'2021-06-30'].drop("1Y_yield",axis=1)
 data = raw_data.replace(".",np.nan).fillna(method = 'ffill')
 
 drop_XLC = [x for x in data.columns if "XLC" in x]
@@ -10,4 +10,4 @@ data.drop(drop_XLC,axis =1, inplace = True)
 
 data = data.astype("float64")
 
-pd.to_csv('')
+pd.to_csv('PredictionIntervals/data/interim/interim_dataset.csv')
