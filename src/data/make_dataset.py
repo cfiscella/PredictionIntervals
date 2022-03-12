2707321208dcec66ef6d5ce2ee7785ec4a9a8362
cfiscella <52965416+cfiscella@users.noreply.github.com>
2022-03-12 00:35:27 -0500

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,1 +1,13 @@
+import numpy as np
+import pandas as pd
 
+raw_data = pd.read_csv('',parse_dates = True,index_col = "DATE",)['2003-12-30':'2021-06-30'].drop("1Y_yield",axis=1)
+data = raw_data.replace(".",np.nan).fillna(method = 'ffill')
+
+drop_XLC = [x for x in data.columns if "XLC" in x]
+
+data.drop(drop_XLC,axis =1, inplace = True)
+
+data = data.astype("float64")
+
+pd.to_csv('')
