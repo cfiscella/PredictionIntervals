dd360bd387bebba1c589d1850158078020b75d43
cfiscella <52965416+cfiscella@users.noreply.github.com>
2022-03-12 23:23:17 -0500

    Update build_features.py

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,1 +1,14 @@
+import numpy as np
+import pandas as pd
 
+interim = pd.read_csv('/data/interim/interim_dataset.csv',parse_dates = True,index_col = "DATE")
+
+etfs = ['XLY',  'XLP', 'XLE', 'XLF', 'XLV', 'XLI', 'XLK', 'XLB', 'IYR', 'XLU']
+
+for etf in etfs:
+  interim[etf+"_target"] = np.log(interim["('Close', '"+etf+"')"]).shift(-1)-np.log(interim["('Close', '"+etf+"')"])
+
+
+preprocessed =interim.iloc[:-1,:]
+
+preprocessed.to_csv('data/processed/processed_dataset.csv')
