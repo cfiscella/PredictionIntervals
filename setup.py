acced5233ab8d1db0fe3a30c9022032caa8dbff6
cfiscella <52965416+cfiscella@users.noreply.github.com>
2022-04-17 19:12:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='PredictionIntervals',
     version='0.1',
-    packages=find_packages(where = 'src')
+    packages=find_packages()
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
