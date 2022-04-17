f5e6d32b6cae937eb30c52bbc106fa7b5a466eee
cfiscella <52965416+cfiscella@users.noreply.github.com>
2022-04-17 19:11:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='PredictionIntervals',
     version='0.1',
-    packages=['model',],
+    packages=find_packages(where = 'src')
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.txt').read(),
 )
