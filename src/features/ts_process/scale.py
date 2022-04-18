a38e0a3bc86af7a8760a8166a8c99f91b5c47656
Christopher Matthew Fiscella <52965416+cfiscella@users.noreply.github.com>
2022-04-18 16:02:59 -0400

    Update scale.py

--- src/features/ts_process/scale.py
+++ src/features/ts_process/scale.py
@@ -1,1 +1,46 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+from src.features.ts_process. import shape_data, dissolve
 
+class WindowStandardScaler(StandardScaler):
+
+  def fit(self,X):
+    X_dissolved = dissolve(X,X.shape[1])
+    StandardScaler.fit(self,X_dissolved)
+
+  def transform(self,X):
+
+    X_dissolved = dissolve(X,X.shape[1])
+    transformed = StandardScaler.transform(self,X_dissolved)
+
+    return shape_data(pd.DataFrame(transformed),X.shape[1])
+
+  def fit_transform(self,X):
+    self.fit(X)
+    return self.transform(X)
+
+  def inverse_transform(self,X):
+    X_dissolved = dissolve(X,X.shape[1])
+    inverse_transformed = StandardScaler.inverse_transform(self,X_dissolved)
+    return shape_data(pd.DataFrame(inverse_transformed),X.shape[1])
+  
+class WindowMinMaxScaler(MinMaxScaler):
+  def fit(self,X):
+    X_dissolved = dissolve(X,X.shape[1])
+    MinMaxScaler.fit(self,X_dissolved)
+
+  def transform(self,X):
+
+    X_dissolved = dissolve(X,X.shape[1])
+    transformed = MinMaxScaler.transform(self,X_dissolved)
+
+    return shape_data(pd.DataFrame(transformed),X.shape[1])
+
+  def fit_transform(self,X):
+    self.fit(X)
+    return self.transform(X)
+
+  def inverse_transform(self,X):
+    X_dissolved = dissolve(X,X.shape[1])
+    inverse_transformed = MinMaxScaler.inverse_transform(self,X_dissolved)
+    return shape_data(pd.DataFrame(inverse_transformed),X.shape[1])
