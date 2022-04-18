7c615694a41401d392ae1bf1348cac222b64bcf3
Christopher Matthew Fiscella <52965416+cfiscella@users.noreply.github.com>
2022-04-18 16:13:40 -0400

    Update baseline.py

--- src/models/model/baseline.py
+++ src/models/model/baseline.py
@@ -1,1 +1,15 @@
-test
+import numpy as np
+
+class interval_baseline:
+  def __init__(self,alpha):
+    self.alpha = alpha
+  def fit(self,X_train,y_train):
+    upper = np.quantile(y_train.flatten(),(1-self.alpha/2))
+    lower = np.quantile(y_train.flatten(),self.alpha/2)
+    self.upper_prediction = upper
+    self.lower_prediction = lower
+  def predict(self,X_test):
+    result = np.empty((len(X_test),2))
+    for i in range(len(X_test)):
+      result[i] = np.array([self.lower_prediction,self.upper_prediction])
+    return result
